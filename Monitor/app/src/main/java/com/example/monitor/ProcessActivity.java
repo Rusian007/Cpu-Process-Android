196b443f3ba9222bfe6caa39db24fa480968defb
Arafat Rahman <masona954@gmail.com>
2022-07-17 18:26:16 +0600

    Internal And External Created

--- Monitor/app/src/main/java/com/example/monitor/ProcessActivity.java
+++ Monitor/app/src/main/java/com/example/monitor/ProcessActivity.java
@@ -7,6 +7,8 @@ import android.graphics.Typeface;
 import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
 import android.os.Bundle;
+import android.os.Environment;
+import android.os.StatFs;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
@@ -22,7 +24,17 @@ import java.util.List;
 import java.util.Map;
 
 public class ProcessActivity extends AppCompatActivity {
-    private TextView text,text3,InternalStgText;
+    private TextView totalInternalSizeText,
+            AvailableInternalSizeText,
+            totalExternalSizeText,
+            AvailableExternalText,
+            text,
+            text3,
+            text6,
+            text7,
+            text8,
+            InternalStgText,
+            ExternalStgText;
     ImageButton backBtn;
 
     @Override
@@ -31,12 +43,49 @@ public class ProcessActivity extends AppCompatActivity {
         setContentView(R.layout.process_main);
 
         Typeface customfontBold = Typeface.createFromAsset(getAssets(),"fonts/TitilliumWeb-Bold.ttf");
+        Typeface customfont = Typeface.createFromAsset(getAssets(),"fonts/TitilliumWeb-Regular.ttf");
+        Typeface customLight = Typeface.createFromAsset(getAssets(),"fonts/TitilliumWeb-Light.ttf");
 
-       // text = findViewById(R.id.textView);
+       text3 = findViewById(R.id.textView3);
+        text6 = findViewById(R.id.textView6);
+        text7 = findViewById(R.id.textView7);
+        text8 = findViewById(R.id.textView8);
         backBtn = (ImageButton) findViewById(R.id.imageButton);
         InternalStgText = findViewById(R.id.InternalStorage);
-        InternalStgText.setTypeface(customfontBold);
+        ExternalStgText = findViewById(R.id.ExternalStorage);
+        totalInternalSizeText = findViewById(R.id.totalInternalSize);
+        AvailableInternalSizeText = findViewById(R.id.AvailableInternalSize);
+        totalExternalSizeText = findViewById(R.id.totalExternalSize);
+        AvailableExternalText = findViewById(R.id.AvailableExternalSize);
 
+        InternalStgText.setTypeface(customfontBold);
+        ExternalStgText.setTypeface(customfontBold);
+        text3.setTypeface(customLight);
+        text6.setTypeface(customLight);
+        text7.setTypeface(customLight);
+        text8.setTypeface(customLight);
+        totalInternalSizeText.setTypeface(customfont);
+        totalExternalSizeText.setTypeface(customfont);
+        AvailableInternalSizeText.setTypeface(customfont);
+        AvailableExternalText.setTypeface(customfont);
+
+        long totalInternal, AvailableInternal, totalExternal, AvailableExternal;
+        totalInternal = TotalInternalStorages();
+        AvailableInternal = AvailableInternalStorages();
+
+        totalExternal = TotalExternalStorages();
+        AvailableExternal = AvailableExternalStorages();
+
+        String totalInternalResult = checkMB_GB(totalInternal);
+        String availableInternalResult = checkMB_GB(AvailableInternal);
+        String totalExternalResult = checkMB_GB(totalExternal);
+        String availableExternalResult = checkMB_GB(AvailableExternal);
+
+        totalInternalSizeText.setText(totalInternalResult);
+        totalExternalSizeText.setText(totalExternalResult);
+
+        AvailableInternalSizeText.setText(availableInternalResult);
+        AvailableExternalText.setText(availableExternalResult);
 
         backBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -57,6 +106,15 @@ public class ProcessActivity extends AppCompatActivity {
 
 
     }
+
+    private String checkMB_GB(long size) {
+        if(size >1000){
+            size = size / 1000;
+            return String.valueOf(size)+" GB";
+        }
+        else return String.valueOf(size+" MB");
+    }
+
     public void openNewActivity(){
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
@@ -96,6 +154,31 @@ public class ProcessActivity extends AppCompatActivity {
 
         return output;
 
+    }
+
+    private long AvailableInternalStorages(){
+        StatFs stat = new StatFs(Environment.getDataDirectory().getPath());
+        long bytesAvailable = (long)stat.getFreeBlocks() * (long)stat.getBlockSize();
+        long megAvailable = bytesAvailable / 1048576;
+        return megAvailable;
+    }
+    private long TotalInternalStorages(){
+        StatFs stat = new StatFs(Environment.getDataDirectory().getPath());
+        long bytesAvailable = (long)stat.getBlockSize() *(long)stat.getBlockCount();
+        long megAvailable = bytesAvailable / 1048576;
+        return megAvailable;
+    }
+    private long AvailableExternalStorages(){
+        StatFs stat = new StatFs(Environment.getExternalStorageDirectory().getPath());
+        long bytesAvailable = (long)stat.getFreeBlocks() * (long)stat.getBlockSize();
+        long megAvailable = bytesAvailable / 1048576;
+        return megAvailable;
+    }
+    private long TotalExternalStorages(){
+        StatFs stat = new StatFs(Environment.getExternalStorageDirectory().getPath());
+        long bytesAvailable = (long)stat.getBlockSize() *(long)stat.getBlockCount();
+        long megAvailable = bytesAvailable / 1048576;
+        return megAvailable;
     }
 
 
